b4f21887c1fd98ffffc0c0812f11934dae629402
Brian <brianqvpham@hotmail.com>
2015-09-27 04:43:37 -0500

    Added getNrg and getFacts to Recyclable

--- iCan/app/src/main/java/com/ican/ican/Recyclable.java
+++ iCan/app/src/main/java/com/ican/ican/Recyclable.java
@@ -12,11 +12,25 @@ public enum Recyclable {
     public int daily;
     public int alltime;
     public double nrg;
+    public static final LAPTOP_RATIO = 20;
+    public static final HAIR_AND_AIR_RATIO = 1;
 
     Recyclable(String name, double nrg) {
         this.name = name;
         this.nrg = nrg;
         daily = 0;
         alltime = 0;
+    }
+
+    public int getNrg() {
+        return nrg;
+    }
+
+    public void getFacts() {
+        int laptop = nrg * LAPTOP_RATIO;
+        int hairAndAir = nrg * HAIR_AND_AIR_RATIO;
+        String fact = ("By recycling, you could charge your laptop for" + laptop + "hours \nOr" +
+                    "use your hairdryer for " + hairAndAir + "hours \nOr use your air conditioner for "
+                    + hairAndAir + " hours.");
     }
 }
