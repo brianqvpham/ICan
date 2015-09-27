0c601c81b2056545b5604ef2a48711b0166449eb
thepaperpilot <thepaperpilot@gmail.com>
2015-09-27 01:18:25 -0500

    Dialog added

--- iCan/app/src/main/java/com/ican/ican/MainActivity.java
+++ iCan/app/src/main/java/com/ican/ican/MainActivity.java
@@ -1,9 +1,12 @@
 package com.ican.ican;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View fab = findViewById(R.id.newRecyclable);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Dialog dialog = new Dialog(MainActivity.this);
+                dialog.setTitle("New Recyclable");
+                dialog.setContentView(R.layout.maindialog);
+                dialog.setCancelable(true);
+
+                View confirm = dialog.findViewById(R.id.confirm);
+                confirm.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                });
+
+                View cancel = dialog.findViewById(R.id.cancel);
+                cancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.cancel();
+                    }
+                });
+
+                dialog.show();
+            }
+        });
     }
 
 
