722038a64dba3c67543471197dd0dd5c4d0419d0
thepaperpilot <thepaperpilot@gmail.com>
2015-09-27 04:02:33 -0500

    Added nrg values to recyclables

--- iCan/app/src/main/java/com/ican/ican/Recyclable.java
+++ iCan/app/src/main/java/com/ican/ican/Recyclable.java
@@ -2,18 +2,20 @@ package com.ican.ican;
 
 public enum Recyclable {
     // TODO make these resource strings?
-    ALUMINUM_CAN("Aluminum Cans"),
-    GLASS_BOTTLE("Glass Bottles"),
-    PLASTIC_BOTTLE("Plastic Bottles"),
-    SHEET_OF_PAPER("Sheets of Paper"),
-    PLASTIC_GROCERY_BAG("Plastic Grocery Bags");
+    ALUMINUM_CAN("Aluminum Cans", .26),
+    GLASS_BOTTLE("Glass Bottles", .1),
+    PLASTIC_BOTTLE("Plastic Bottles", .13),
+    SHEET_OF_PAPER("Sheets of Paper", .05),
+    PLASTIC_GROCERY_BAG("Plastic Grocery Bags", .02);
 
     public String name;
     public int daily;
     public int alltime;
+    public double nrg;
 
-    Recyclable(String name) {
+    Recyclable(String name, double nrg) {
         this.name = name;
+        this.nrg = nrg;
         daily = 0;
         alltime = 0;
     }
